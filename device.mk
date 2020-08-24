b6726e4eaa4db4083b88cd18a9d3c05af2ef6da2
SGCMarkus <markusornik@gmail.com>
2020-08-24 17:11:05 +0200

    tundra: implement lineage touch hal

--- device.mk
+++ device.mk
@@ -86,6 +86,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Touch
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.tundra
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
