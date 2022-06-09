bf451a686cb1b8dfa56bd551c1ba7e8858668480
Bruno Martins <bgcngm@gmail.com>
2022-06-09 14:34:59 +0100

    tundra: Use minimal GMS if building with it
    * ROM getting too big to upload...
    
    Change-Id: If9ce99880bcd6949354f80882176aaaded9aad85

--- device.mk
+++ device.mk
@@ -49,6 +49,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.tundra
 
+# GMS
+ifeq ($(WITH_GMS),true)
+GMS_MAKEFILE=gms_minimal.mk
+endif
+
 # Init
 $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
         $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
