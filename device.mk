6ba205c7661f461f5eff5754415eac64a062e487
Marc Bourgoin <themard85@gmail.com>
2024-12-22 00:24:11 -0700

    tundra: Declare we have SNXXX NFC AIDL HAL
    This way our sm7325-common tree doesn't try to declare AOSP NFC HAL
    
    Change-Id: Ib16f616785616141505a81c52d8c4cfd7f8ecd07

--- device.mk
+++ device.mk
@@ -1,17 +1,7 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # A/B
@@ -23,6 +13,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Screen
 TARGET_SCREEN_DENSITY := 400
+
+# NFC
+TARGET_HAS_SNXXX_NFC := true
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
